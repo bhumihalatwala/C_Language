9b252b815958b7c5bf4423a0a88c28e5f9789bbb
bhumihalatwala <bhumihalatwala2601@gmail.com>
2024-12-30 10:55:35 +0530

    Print name

---
+++ NAME.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+  clrscr();
+  printf("My name is Bhumi Halatwala.\n");
+  printf("I am 18 years old.\n");
+  printf("I studied from Radiant English Academy.");
+
+  getch();
+}
