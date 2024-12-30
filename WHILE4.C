0c01f8472812c72e606d0ec442fb44c58b08d546
bhumihalatwala <bhumihalatwala2601@gmail.com>
2024-12-30 11:14:00 +0530

    print odd numbers from n to 1 using while loop

---
+++ WHILE4.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+//print odd numbers from n to 1 using while loop
+main()
+{
+  int n;
+  clrscr();
+
+  printf("Enter any number: ");
+  scanf("%d",&n);
+
+  while(n>=1)
+  {
+    if(n%2 == 1)
+    {
+      printf("%d\t",i);
+    }
+    i--;
+  }
+  getch();
+}
