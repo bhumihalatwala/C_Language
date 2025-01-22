dae161e07e0c4bf5df0e9123b8ccf834a66622e1
bhumihalatwala <bhumihalatwala2601@gmail.com>
2025-01-22 09:35:18 +0530

    Convert given string to uppercase

---
+++ STRING1.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+    char str[100];
+
+    printf("Enter any string: ");
+    scanf("%s",str);
+
+    for (int i = 0; str[i] != '\0'; i++) 
+    {
+        if (str[i] >= 'a' && str[i] <= 'z') 
+        {
+            str[i] = str[i] - 32; // Convert lowercase to uppercase
+        }
+    }
+
+    printf("Uppercase string: %s\n", str);
+
+    return 0;
+}
