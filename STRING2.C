9bece3f1c36be965eb930a343293df574856714e
bhumihalatwala <bhumihalatwala2601@gmail.com>
2025-01-22 09:35:42 +0530

    Convert given string to lowercase

---
+++ STRING2.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+    char str[100];
+
+    printf("Enter any string: ");
+    scanf("%s",str);
+
+    for (int i = 0; str[i] != '\0'; i++) 
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z') 
+        {
+            str[i] = str[i] + 32; // Convert lowercase to uppercase
+        }
+    }
+
+    printf("Lowercase string: %s\n", str);
+
+    return 0;
+}
